15b550bba7a8c0d44f2d3a007f49b178831fc68c
Rodrigo Saito <rodrigo.saito@gmail.com>
2015-12-25 14:07:50 -0200

    Update scripts

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -1,13 +1,8 @@
-#!/bin/bash
-
-# Vagrant specific
 date > /etc/vagrant_box_build_time
 
-# Installing vagrant keys
-mkdir -pm 700 /home/vagrant/.ssh
-wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O /home/vagrant/.ssh/authorized_keys
-chmod 0600 /home/vagrant/.ssh/authorized_keys
+mkdir /home/vagrant/.ssh
+wget --no-check-certificate \
+    'https://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub' \
+    -O /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
-
-# Customize the message of the day
-echo 'Development Environment' > /etc/motd
+chmod -R go-rwsx /home/vagrant/.ssh
