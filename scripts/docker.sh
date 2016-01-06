88805470b7afb192e259b7150ea93c66252b6702
Rodrigo Saito <rodrigo.saito@gmail.com>
2016-01-06 07:32:32 -0200

    Added synced folders and install docker-compose

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -2,3 +2,7 @@ sudo apt-get update -qq -y
 sudo apt-get install -qq -y --force-yes curl apt-transport-https
 sudo apt-get purge -qq -y lxc-docker* || true
 curl -sSL https://get.docker.com/ | sudo sh
+
+# install docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
