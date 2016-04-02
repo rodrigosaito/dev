858aa08808bafd35682481dd4409e85c852c14bd
Rodrigo Saito <rodrigo.saito@gmail.com>
2016-04-01 22:11:05 -0300

    Update versions

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -4,5 +4,5 @@ sudo apt-get purge -qq -y lxc-docker* || true
 curl -sSL https://get.docker.com/ | sudo sh
 
 # install docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
