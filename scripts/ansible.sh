746f6c0efedbe711d7ceb7e06d5ab21e258ad9f0
Rodrigo Saito <rodrigo.saito@gmail.com>
2016-04-02 09:37:32 -0300

    Fix nodejs and golang roles

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -2,3 +2,4 @@ sudo apt-get install -y software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
+ansible-galaxy install nodesource.node
